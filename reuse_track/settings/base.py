91dcaa420b7327e12e14bacc50040c102b22184e
Srimsd77 <126162438+Srimsd77@users.noreply.github.com>
2025-05-22 11:35:36 +0530

    Update base.py

--- reuse_track/settings/base.py
+++ reuse_track/settings/base.py
@@ -148,9 +148,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 # Static files
-STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR / 'static']
-
+STATIC_URL = '/staticfiles/'
+#STATICFILES_DIRS = [BASE_DIR / 'static']
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # Media files
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
